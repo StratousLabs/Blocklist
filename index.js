d5e4068b0761c3b331fe0850150b11f26d9aa5ab
BluePr1nt-X <71233060+BluePr1nt-X@users.noreply.github.com>
2024-08-04 17:34:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,22 +7,8 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const allowedOrigins = ['chrome-extension://mflodnknmehgofdjgeghipoihhifkchl'];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps, curl requests)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-};
-
-// Use CORS middleware to allow cross-origin requests from specific origins
-app.use(cors(corsOptions));
+// Use CORS middleware to allow cross-origin requests
+app.use(cors());
 
 let blacklist = new Set();
 
